34dae07579dba9d0ea9c04d465fd37d6fcdb342b
Waswa Rodgers <waswam1994@gmail.com>
2020-07-25 18:55:40 +0300

    Modified index.php

---
+++ includes/footer.php
@@ -0,0 +1,80 @@
+<!-- Footer -->
+<footer>
+            <!-- Footer top -->
+            <div class="container footer_top">
+                <div class="row">
+                    <div class="col-lg-4 col-sm-7">
+                        <div class="footer_item">
+                            <h4>About Bondlink Experts Co. Ltd</h4>
+                            <p>We Process Bid bonds, Performance bonds and Advanced Guarantee Payments at friendly prices from highly reputable banks and insurance companies</p>
+
+                            <ul class="list-inline footer_social_icon">
+                                <li><a href=""><span class="fa fa-facebook"></span></a></li>
+                                <li><a href=""><span class="fa fa-twitter"></span></a></li>
+                                <li><a href=""><span class="fa fa-linkedin"></span></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-sm-5">
+                        <div class="footer_item">
+                            <h4>Explore link</h4>
+                            <ul class="list-unstyled footer_menu">
+                                <li><a href=""><span class="fa fa-play"></span> Our services</a>
+                                <li><a href=""><span class="fa fa-play"></span> Meet our team</a>
+                                <li><a href=""><span class="fa fa-play"></span> Forum</a>
+                                <li><a href=""><span class="fa fa-play"></span> Help center</a>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-7">
+                        <div class="footer_item">
+                            <h4>Latest post</h4>
+                            <ul class="list-unstyled post">
+                                <li><a href=""><span class="date">20 <small>AUG</small></span>  Bondlink Experts CO. Ltd emerges the best...</a></li>
+                                <li><a href=""><span class="date">20 <small>AUG</small></span>  We just increased number of customers...</a></li>
+                                
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-5">
+                        <div class="footer_item">
+                            <h4>Contact Us</h4>
+                            <ul class="list-unstyled footer_contact">
+                                <li><a href=""><span class="fa fa-map-marker"></span>3rd Flr. Utalii Hse, Utalii Street P.O BOX 1184 - 00100 Nairobi</a></li>
+                                <li><a href=""><span class="fa fa-envelope"></span> finance@bondlinkexperts.co.ke</a></li>
+                                <li><a href=""><span class="fa fa-mobile"></span><p>+254 715877238 <br />+254 716225693</p></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div><!-- Footer top end -->
+
+            <!-- Footer bottom -->
+            <div class="footer_bottom text-center">
+                <p class="wow fadeInRight">
+                    Made with 
+                    <i class="fa fa-heart"></i>
+                    by 
+                    <a target="_blank" href="https://waswarodgers.wordpress.com">Waswa Rodgers</a> 
+                    2020. All Rights Reserved
+                </p>
+            </div><!-- Footer bottom end -->
+        </footer><!-- Footer end -->
+
+        <!-- JavaScript -->
+        <script src="js/jquery-1.12.1.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+
+        <!-- Bootsnav js -->
+        <script src="js/bootsnav.js"></script>
+
+        <!-- JS Implementing Plugins -->
+        <script src="js/isotope.js"></script>
+        <script src="js/isotope-active.js"></script>
+        <script src="js/jquery.fancybox.js?v=2.1.5"></script>
+
+        <script src="js/jquery.scrollUp.min.js"></script>
+
+        <script src="js/main.js"></script>
+    </body>	
+</html>	
