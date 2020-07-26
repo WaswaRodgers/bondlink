210d4729cd922dc5a09e0a468ec4e14b8148ea84
Waswa Rodgers <waswam1994@gmail.com>
2020-07-26 15:23:53 +0300

    Updated about page information

--- about.php
+++ about.php
@@ -1,2 +1,32 @@
 <?php include_once('includes/header.php'); ?>
+
+<!-- About -->
+<section id="about">
+            <div class="container about_bg">
+                <div class="row">
+                    <div class="col-lg-7 col-md-6">
+                        <div class="about_content">
+                            <h2>About Our Company</h2>
+                            <p>We Process Bid bonds, Performance bonds and Advanced Guarantee Payments at friendly prices from highly reputable banks and insurance companies</p>
+                            <p>We Process Bid bonds, Performance bonds and Advanced Guarantee Payments at friendly prices from highly reputable banks and insurance companies</p>
+                            <span id="dots1">...</span>
+                            
+                            <span id="more1">We Process Bid bonds, Performance bonds and Advanced Guarantee Payments at friendly prices from highly reputable banks and insurance companies. 
+                                    We Process Bid bonds, Performance bonds and Advanced Guarantee Payments at friendly prices from highly reputable banks and insurance companies. 
+                                    We Process Bid bonds, Performance bonds and Advanced Guarantee Payments at friendly prices from highly reputable banks and insurance companies. 
+                                    We Process Bid bonds, Performance bonds and Advanced Guarantee Payments at friendly prices from highly reputable banks and insurance companies</span></p>
+                                <!--<a  class="btn know_btn">Read More</a>-->
+                                <button onclick = "handleReadMoreLess()" id="myBtn1" class="btn bg-primary text-white">Read more</button>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-lg-offset-1">
+                        <div class="about_banner">
+                            <img src="images/man.png" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- About end -->
+
 <?php include_once('includes/footer.php'); ?>
