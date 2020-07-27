0248235c9e4e832faab35f9fbf92c8febce9841e
Waswa Rodgers <waswam1994@gmail.com>
2020-07-27 21:41:53 +0300

    Added team page file

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 "use strict";
-jQuery(document).ready(function ($) {
+$(document).ready(function () {
 
 //==========================================
 // MOBAILE MENU
@@ -56,24 +56,6 @@ jQuery(document).ready(function ($) {
     $(window).load(function () {
         $("#loading").fadeOut(500);
     });
-
-
-    function handleReadMoreLess() {
-        var dots = document.getElementById('dots');
-        var moreText = document.getElementById('more');
-        var btnText = document.getElementById('myBtn');
-        
-        if(dots.style.display === "none") {
-            dots.style.display = "inline";
-            btnText.innerHTML = "Read more";
-            moreText.style.display = "none";
-        } else {
-            dots.style.display = "none";
-            btnText.innerHTML = "Read less";
-            moreText.style.display = "inline";
-        }
-    }
-
 
 
 });
--- team.php
+++ team.php
@@ -10,4 +10,21 @@
     include_once('includes/header.php'); 
     
 ?>
+
+<section>
+	<div class="container">
+		<h1>Meet The Team</h1>
+		<div class="card-wrapper">
+			<div class="card">
+				<img src="images/service_img1.png" alt="Card-background" class="card-img">
+				<img src="images/service_img1.png" alt="Profile-image" class="profile-img">
+				<h2>Waswa Rodgers</h2>
+				<p class="job-title">Web Designer</p>
+				<p class="about">The guy is cool. The guy is cool. The guy is cool. The guy is cool. The guy is cool.</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+
 <?php include_once('includes/footer.php'); ?>
